147226e492bb6e990e1742317b6e4445c0d1eb5b
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-12-14 13:25:06 -0700

    Create fast money sub class

---
+++ src/fast-money.js
@@ -0,0 +1,18 @@
+class FastMoney extends Round {
+  constructor(survey, player1, player2) {
+    super(survey, player1, player2)
+    this.timer = number ;
+    this.multiplier = number; // 1 - 5
+  }
+
+    startRound() { // starts timer setsMultiplier()
+
+  }
+
+  setsMultiplier() {
+
+  }
+}
+
+
+export default FastMoney;

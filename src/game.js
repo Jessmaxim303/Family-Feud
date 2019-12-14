72b696d096bd7121a517fb3a0da3945fd7fb2d0f
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-12-14 13:27:23 -0700

    Create game class

---
+++ src/game.js
@@ -0,0 +1,28 @@
+class Game {
+  constructor() {
+    this.player1 = {};
+    this.player2 = {};
+    this.scoreboard  = [];
+    this.round = {};
+    this.roundCount = 0; // might not need 
+  }
+
+  returnWinner() {
+
+  }
+
+  fastMoney() {
+
+  }
+
+  createRound() {
+
+  }
+
+  createPlayer() {
+
+  }
+}
+
+
+export default Game;

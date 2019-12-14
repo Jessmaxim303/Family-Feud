1cf63530ba88f5ee48c35c12ed172cf93da4b639
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-12-14 13:25:34 -0700

    Create player class

---
+++ src/player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+    this.turnActive = false;
+  }
+
+  scoreTracker() {
+
+  }
+
+  endGame() {
+
+  }
+}
+
+export default Player;

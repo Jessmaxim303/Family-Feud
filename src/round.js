a80c15eac5d56c8e49a513a1df3376bf426fe5f4
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-12-14 13:27:01 -0700

    Create round class

---
+++ src/round.js
@@ -0,0 +1,29 @@
+class Round {
+  constructor(survey, player1, player2) {
+    this.survey = [{}]
+    this.surveyId = survey.id
+    this.question = survey.question;
+    this.answers = [];
+    this.respondents = answer.respondents;
+    this.activePlayer = {};
+    this.feedback = [];
+  }
+
+  randomSurveyQuestion() {
+
+  }
+
+  checkQuestion() {
+
+  }
+
+  returnFeedback() {
+
+  }
+
+  submitNewGuess() {
+
+  }
+}
+
+export default Round;

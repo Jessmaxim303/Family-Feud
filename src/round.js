fc636e704aa9b1ccd5258c58bfd57c2f4366f267
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-06 12:15:45 -0700

    Change methods to eliminate survey options

--- src/round.js
+++ src/round.js
@@ -5,14 +5,16 @@ class Round {
     this.question = survey.question;
     this.answers = survey.answers;
     this.respondents = survey.respondents;
-    this.activePlayer = player1;
     this.feedback = [];
     this.correctGuesses = 0;
+    this.surveyChances =
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   }
 
   randomSurveyQuestion() {
-    this.id = Math.ceil(Math.random() * this.survey.length);
-    return this.survey.filter(x => (x.id === this.id));
+    this.id = Math.ceil(Math.random() * this.surveyChances.length);
+    this.surveyChances.splice(this.id - 1, 1);
+    return this.survey[this.id - 1];
   }
 
   getAnswerById() {

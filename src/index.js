b6348f6e1d7e274b5b96482f5da0b60833c39609
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-06 11:23:53 -0700

    Add turn functionality

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import Player from "./player";
 
 $(".main_section").hide();
 
+let round;
+let game;
+
 const showGameBoard = () => {
   if ($(".main_p1-log").val() && $(".main_p2-log").val()) {
     $(".main_login").hide();
@@ -17,9 +20,36 @@ const showGameBoard = () => {
   };
 };
 
+const resetGuesses = () => {
+  round.correctGuesses = 0;
+  game.player1.incorrectGuessCount = 0;
+  game.player2.incorrectGuessCount = 0;
+}
+
+const removeXs = () => {
+  if(!$(".player1_strike-1").hasClass("hidden")) {
+    $(".player1_strike-1").addClass("hidden");
+  }
+  if(!$(".player1_strike-2").hasClass("hidden")) {
+    $(".player1_strike-2").addClass("hidden");
+  }
+  if(!$(".player1_strike-3").hasClass("hidden")) {
+    $(".player1_strike-3").addClass("hidden");
+  }
+  if(!$(".player2_strike-1").hasClass("hidden")) {
+    $(".player2_strike-1").addClass("hidden");
+  }
+  if(!$(".player2_strike-2").hasClass("hidden")) {
+    $(".player2_strike-2").addClass("hidden");
+  }
+  if(!$(".player2_strike-3").hasClass("hidden")) {
+    $(".player2_strike-3").addClass("hidden");
+  }
+}
+
 const newRound = () => {
   $(".main_answer-section").empty();
-  $(".main_question-section").text(round.randomSurveyQuestion()[0].question);
+  $(".main_question-section").text(round.randomSurveyQuestion().question);
   round.getAnswerById().forEach(answer => {
     $(".main_answer-section").append(
       `<section class="answer_container answer-cover ${answer.answer.toLowerCase().split(' ').join('')}">
@@ -27,11 +57,12 @@ const newRound = () => {
             <h3 class="answer_container-text answer-score">${answer.respondents}</h3>
        </section>`);
   });
+  resetGuesses();
+  removeXs();
 }
 
 $(".main-login-submit").click(showGameBoard);
-let round;
-let game;
+
 const startGame = (data) => {
   game = new Game(data);
   game.createPlayers();
@@ -39,7 +70,28 @@ const startGame = (data) => {
   newRound();
 }
 
+const evaluateTurn = (player) => {
+  const player1Input = $(".p1_guess-input");
+  const player1Button = $(".p1_guess-button");
+  const player2Input = $(".p2_guess-input");
+  const player2Button = $(".p2_guess-button");
+  if(player === 'player1') {
+    player1Input.prop("disabled", true);
+    player2Input.prop("disabled", false);
+    player1Input.val("");
+    player2Button.prop("disabled", false);
+    player1Button.prop("disabled", true);
+  } else if (player === 'player2') {
+    player2Input.prop("disabled", true);
+    player1Input.prop("disabled", false);
+    player2Input.val("");
+    player1Button.prop("disabled", false);
+    player2Button.prop("disabled", true);
+  }
+}
+
 const getGuess = (guess, player) => {
+  evaluateTurn(player);
   game[player].guessCount++;
   if(round.checkQuestion(guess)) {
     $(`.${guess}`).addClass("flip_answer");
@@ -50,15 +102,10 @@ const getGuess = (guess, player) => {
     game[player].incorrectGuessCount++;
     $(`.${player}_strike-${game.player1.incorrectGuessCount}`).removeClass("hidden");
   }
-  if(round.activePlayer === game.player1) {
-    round.activePlayer = game.player2;
-  } else {
-    round.activePlayer = game.player1;
-  }
-  console.log(round.correctGuesses);
-
-  if(round.correctGuesses === 6 || game.player1.incorrectGuessCount === 3 &&game.player2.incorrectGuessCount === 3) {
-    newRound();
+  if(round.correctGuesses >= 6 ||
+    game.player1.incorrectGuessCount >= 3 &&
+    game.player2.incorrectGuessCount >= 3) {
+      newRound();
   }
 };
 
@@ -76,8 +123,6 @@ const sendGuess = () => {
   getGuess($(".p2_guess-input").val().toLowerCase().split(' ').join(''), 'player2');
   }
 };
-
-
 
 receiveData();
 

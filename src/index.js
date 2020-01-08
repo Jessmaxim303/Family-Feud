12348f6cd7e8afbd87a242b6e1956c44e8f7a8e5
Jesse Maxim <45706207+Jessmaxim303@users.noreply.github.com>
2020-01-08 08:53:14 -0700

    Revert "Scoreboard"

--- src/index.js
+++ src/index.js
@@ -58,13 +58,10 @@ const getScoreBoardData = () => {
 const showScoreBoard = (scores) => {
   let highScores = scores.highScores.filter(score => score.appId === "1909knthjm").sort((a, b) => a.score - b.score)
   $(".main_highscores-section").empty();
-  $(".main_highscores-section").append(`HIGH SCORES:`);
-  $(".main_highscores-section").removeClass("hidden");
-  let num = 0;
+  $(".main_highscores-section").append(`HIGH SCORES:`)
   highScores.forEach(score => {
-    num++;
     $(".main_highscores-section").append(`
-      <h4>#${num}. ${score.playerName} with ${score.playerScore} points
+      <h4>${score.playerName} with ${score.playerScore} points
     `)
   })
 }
@@ -84,14 +81,13 @@ const postWinner = (player) => {
 const newRound = () => {
   game.roundCount++;
   if (game.roundCount === 3) {
-    moneyRound();
+    moneyRound()
   }
-  let answerSection = $(".main_answer-section");
+  let answerSection = $(".main_answer-section")
   answerSection.empty();
   $(".main_question-section").empty();
+  console.log(round.survey);
   if(round.survey.length <= 0) {
-    $(".reset-button").removeClass("hidden");
-    $(".main_highscores-button").removeClass("hidden");
     if(game.player1.score > game.player2.score) {
       $(".main_question-section").text(`Game Over, ${$(".main_player1-log").val()} wins with ${game.player1.score} points`);
       postWinner("player1");
@@ -160,7 +156,7 @@ const getGuess = (guess, player) => {
   if(round.correctGuesses >= 6 ||
     game.player1.incorrectGuessCount >= 3 &&
     game.player2.incorrectGuessCount >= 3) {
-      setTimeout(newRound, 1200);
+      setTimeout(newRound, 1500);
   }
 };
 
@@ -179,26 +175,19 @@ const sendGuess = () => {
   }
 };
 
-let timeLeft = 30;
-var timerBox = document.querySelector('.js_timer-box');
-var jsTimerText = document.querySelector('.js_timer-text')
-
 const moneyRound = () => {
   $('.js_money-header').text('Money Round!')
   var timerId = setInterval(countdown, 1000);
 }
-
-const countdown = () => {
-  if (timeLeft === 0) {
-    timerBox.innerHTML = '';
-    jsTimerText.innerHTML = '';
-    $('.js_money-header').empty();
-  } else {
-    timerBox.innerHTML = timeLeft;
-    jsTimerText.innerHTML = ' Seconds Remaining'
-    timeLeft--;
-  }
-}
+    
+function countdown() {
+      if (timeLeft == 0) {
+      } else {
+        $('.js_timer-box').text(timeLeft)
+        $('.js_timer-text').text(' Seconds Remaining')
+        timeLeft--;
+      }
+    }
 
 receiveData();
 
@@ -206,4 +195,4 @@ $(".p1_guess-button").click(sendGuess);
 $(".p2_guess-button").click(sendGuess);
 $(".main-login-submit").click(showGameBoard);
 $(".main_highscores-button").click(getScoreBoardData);
-$(".main_reset-button").click(resetGame);
+
